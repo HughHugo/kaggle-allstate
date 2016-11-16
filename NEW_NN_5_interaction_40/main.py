66f54d53178ce293ade15ae6d20cd2a30df180b6
achm6174 <achm6174@gmail.com>
2016-11-16 20:22:01 +0800

    update

---
+++ NEW_NN_5_interaction_40/main.py
@@ -0,0 +1,319 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+np.random.seed(6174)
+import pandas as pd
+import subprocess
+from scipy.sparse import csr_matrix, hstack
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import KFold
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers.advanced_activations import PReLU
+from keras.callbacks import EarlyStopping
+from keras.layers.normalization import BatchNormalization
+
+## Batch generators ##################################################################################################################################
+
+def batch_generator(X, y, batch_size, shuffle):
+    #chenglong code for fiting from generator (https://www.kaggle.com/c/talkingdata-mobile-user-demographics/forums/t/22567/neural-network-for-sparse-matrices)
+    number_of_batches = np.ceil(X.shape[0]/batch_size)
+    counter = 0
+    sample_index = np.arange(X.shape[0])
+    if shuffle:
+        np.random.shuffle(sample_index)
+    while True:
+        batch_index = sample_index[batch_size*counter:batch_size*(counter+1)]
+        X_batch = X[batch_index,:].toarray()
+        y_batch = y[batch_index]
+        counter += 1
+        yield X_batch, y_batch
+        if (counter == number_of_batches):
+            if shuffle:
+                np.random.shuffle(sample_index)
+            counter = 0
+
+def batch_generatorp(X, batch_size, shuffle):
+    number_of_batches = X.shape[0] / np.ceil(X.shape[0]/batch_size)
+    counter = 0
+    sample_index = np.arange(X.shape[0])
+    while True:
+        batch_index = sample_index[batch_size * counter:batch_size * (counter + 1)]
+        X_batch = X[batch_index, :].toarray()
+        counter += 1
+        yield X_batch
+        if (counter == number_of_batches):
+            counter = 0
+
+########################################################################################################################################################
+
+import gc
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.metrics import mean_absolute_error
+from sklearn.cross_validation import KFold
+from scipy.stats import skew, boxcox
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+import itertools
+
+
+shift = 200
+SHIFT = 200
+SEED = 6174
+COMB_FEATURE = 'cat80,cat87,cat57,cat12,cat79,cat10,cat7,cat89,cat2,cat72,cat81,cat11,cat1,cat13,cat9,cat3,cat16,cat90,cat23,cat36,cat73,cat103,cat40,cat28,cat111,cat6,cat76,cat50,cat5,cat4,cat14,cat38,cat24,cat82,cat25'.split(
+    ',')
+################################################################################
+
+ID = 'id'
+TARGET = 'loss'
+SEED = 6174
+DATA_DIR = "../input"
+
+TRAIN_FILE = "{0}/train.csv".format(DATA_DIR)
+TEST_FILE = "{0}/test.csv".format(DATA_DIR)
+SUBMISSION_FILE = "{0}/sample_submission.csv".format(DATA_DIR)
+
+
+train = pd.read_csv(TRAIN_FILE)
+test = pd.read_csv(TEST_FILE)
+
+SHIFT = 200
+################################################################################
+skf_table = pd.read_csv('../cache/stack_index.csv')
+skf_table = pd.merge(train, skf_table, on='id')
+skf = []
+nfold = 10
+for i in range(nfold):
+    skf += [[(skf_table['stack_index']!=i).values, (skf_table['stack_index']==i).values]]
+print skf
+print len(skf)
+label = np.log(train['loss'].values + SHIFT)
+trainID = train['id']
+################################################################################
+################################################################################
+
+def encode(charcode):
+    r = 0
+    if(type(charcode) is float):
+        return np.nan
+    else:
+        ln = len(charcode)
+        for i in range(ln):
+            r += (ord(charcode[i]) - ord('A') + 1) * 26 ** (ln - i - 1)
+        return r
+
+
+def logregobj(preds, dtrain):
+    labels = dtrain.get_label()
+    con = 2
+    x = preds - labels
+    grad = con * x / (np.abs(x) + con)
+    hess = con ** 2 / (np.abs(x) + con) ** 2
+    return grad, hess
+
+
+def xg_eval_mae(yhat, dtrain):
+    y = dtrain.get_label()
+    return 'mae', mean_absolute_error(np.exp(y) - shift,
+                                      np.exp(yhat) - shift)
+
+
+def mungeskewed(train, test, numeric_feats):
+    ntrain = train.shape[0]
+    test['loss'] = 0
+    train_test = pd.concat((train, test)).reset_index(drop=True)
+    # compute skew and do Box-Cox transformation (Tilli)
+    skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna()))
+    print("\nSkew in numeric features:")
+    print(skewed_feats)
+    skewed_feats = skewed_feats[skewed_feats > 0.25]
+    skewed_feats = skewed_feats.index
+
+    for feats in skewed_feats:
+        train_test[feats] = train_test[feats] + 1
+        train_test[feats], lam = boxcox(train_test[feats])
+    return train_test, ntrain
+
+
+print('Started')
+directory = '../input/'
+train = pd.read_csv(directory + 'train.csv')
+test = pd.read_csv(directory + 'test.csv')
+
+
+id_train = train['id'].values
+id_test = test['id'].values
+numeric_feats = [x for x in train.columns[1:-1] if 'cont' in x]
+cats = [x for x in train.columns[1:-1] if 'cat' in x]
+train_test, ntrain = mungeskewed(train, test, numeric_feats)
+
+# taken from Vladimir's script (https://www.kaggle.com/iglovikov/allstate-claims-severity/xgb-1114)
+for column in list(train.select_dtypes(include=['object']).columns):
+    if train[column].nunique() != test[column].nunique():
+        set_train = set(train[column].unique())
+        set_test = set(test[column].unique())
+        remove_train = set_train - set_test
+        remove_test = set_test - set_train
+
+        remove = remove_train.union(remove_test)
+
+
+        def filter_cat(x):
+            if x in remove:
+                return np.nan
+            return x
+
+
+        train_test[column] = train_test[column].apply(lambda x: filter_cat(x), 1)
+
+# taken from Ali's script (https://www.kaggle.com/aliajouz/allstate-claims-severity/singel-model-lb-1117)
+train_test["cont1"] = np.sqrt(preprocessing.minmax_scale(train_test["cont1"]))
+train_test["cont4"] = np.sqrt(preprocessing.minmax_scale(train_test["cont4"]))
+train_test["cont5"] = np.sqrt(preprocessing.minmax_scale(train_test["cont5"]))
+train_test["cont8"] = np.sqrt(preprocessing.minmax_scale(train_test["cont8"]))
+train_test["cont10"] = np.sqrt(preprocessing.minmax_scale(train_test["cont10"]))
+train_test["cont11"] = np.sqrt(preprocessing.minmax_scale(train_test["cont11"]))
+train_test["cont12"] = np.sqrt(preprocessing.minmax_scale(train_test["cont12"]))
+
+train_test["cont6"] = np.log(preprocessing.minmax_scale(train_test["cont6"]) + 0000.1)
+train_test["cont7"] = np.log(preprocessing.minmax_scale(train_test["cont7"]) + 0000.1)
+train_test["cont9"] = np.log(preprocessing.minmax_scale(train_test["cont9"]) + 0000.1)
+train_test["cont13"] = np.log(preprocessing.minmax_scale(train_test["cont13"]) + 0000.1)
+train_test["cont14"] = (np.maximum(train_test["cont14"] - 0.179722, 0) / 0.665122) ** 0.25
+
+
+for comb in itertools.combinations(COMB_FEATURE, 2):
+    feat = comb[0] + "_" + comb[1]
+    train_test[feat] = train_test[comb[0]] + train_test[comb[1]]
+    train_test[feat] = train_test[feat].apply(encode)
+    print(feat)
+
+
+cats = [x for x in train.columns[1:-1] if 'cat' in x]
+for col in cats:
+    train_test[col] = train_test[col].apply(encode)
+train_test.loss = np.log(train_test.loss + shift)
+ss = StandardScaler()
+train_test[numeric_feats] = \
+    ss.fit_transform(train_test[numeric_feats].values)
+train = train_test.iloc[:ntrain, :].copy()
+label = train.loss.values
+test = train_test.iloc[ntrain:, :].copy()
+
+
+
+## set test loss to NaN
+test['loss'] = np.nan
+
+## response and IDs
+y = train['loss'].values
+
+## stack train test
+ntrain = train.shape[0]
+tr_te = pd.concat((train, test), axis = 0)
+
+## Preprocessing and transforming to sparse data
+sparse_data = []
+
+f_cat = [f for f in tr_te.columns if 'cat' in f]
+for f in f_cat:
+    dummy = pd.get_dummies(tr_te[f].astype('category'))
+    tmp = csr_matrix(dummy)
+    sparse_data.append(tmp)
+
+f_num = [f for f in tr_te.columns if 'cont' in f]
+scaler = StandardScaler()
+tmp = csr_matrix(scaler.fit_transform(tr_te[f_num]))
+sparse_data.append(tmp)
+
+del(tr_te, train, test)
+
+## sparse train and test data
+xtr_te = hstack(sparse_data, format = 'csr')
+xtrain = xtr_te[:ntrain, :]
+xtest = xtr_te[ntrain:, :]
+
+print('Dim train', xtrain.shape)
+print('Dim test', xtest.shape)
+
+del(xtr_te, sparse_data, tmp)
+
+## neural net
+def nn_model():
+    model = Sequential()
+
+    model.add(Dense(400, input_dim = xtrain.shape[1], init = 'he_normal'))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+
+    model.add(Dense(200, init = 'he_normal'))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+
+    model.add(Dense(50, init = 'he_normal'))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+
+    model.add(Dense(1, init = 'he_normal'))
+    model.compile(loss = 'mae', optimizer = 'adadelta')
+    return(model)
+
+## cv-folds
+nfolds = nfold
+folds = skf
+
+## train models
+i = 0
+nbags = 5
+nepochs = 40
+pred_oob = np.zeros(xtrain.shape[0])
+pred_test = np.zeros(xtest.shape[0])
+
+early_stopping = EarlyStopping(monitor='val_loss',
+                               patience=3,
+                               mode="min")
+
+for inTr, inTe in folds:
+    print inTr
+    print inTe
+    inTr = np.where(inTr)
+    inTe = np.where(inTe)
+    print inTr
+    print inTe
+    xtr = xtrain[inTr]
+    ytr = y[inTr]
+    xte = xtrain[inTe]
+    yte = y[inTe]
+    pred = np.zeros(xte.shape[0])
+    for j in range(nbags):
+        model = nn_model()
+        fit = model.fit_generator(generator = batch_generator(xtr, ytr, 128, True),
+                                  nb_epoch = nepochs,
+                                  samples_per_epoch = xtr.shape[0],
+                                  validation_data = (xte.todense(), yte),
+                                  verbose = 1)
+        pred += np.exp(model.predict_generator(generator = batch_generatorp(xte, 800, False),
+                                        val_samples = xte.shape[0])[:,0]) -SHIFT
+        pred_test += np.exp(model.predict_generator(generator = batch_generatorp(xtest, 800, False),
+                                        val_samples = xtest.shape[0])[:,0]) -SHIFT
+    pred /= nbags
+    pred_oob[inTe] = pred
+    score = mean_absolute_error(np.exp(yte)-SHIFT, pred)
+    i += 1
+    print('Fold ', i, '- MAE:', score)
+
+print('Total - MAE:', mean_absolute_error(y, pred_oob))
+
+## train predictions
+df = pd.DataFrame({'id': id_train, 'loss': pred_oob})
+df.to_csv('NN_retrain_1.csv', index = False)
+
+## test predictions
+pred_test /= (nfolds*nbags)
+df = pd.DataFrame({'id': id_test, 'loss': pred_test})
+df.to_csv('NN_1.csv', index = False)
